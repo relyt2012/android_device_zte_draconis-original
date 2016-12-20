b12fe3d34ca2994ed439cef792e08e9bba838cbe
Tyler <tyler.android12@gmail.com>
2016-12-19 21:23:41 -0500

    Removed ffmpeg.xml

--- full_draconis.mk
+++ full_draconis.mk
@@ -171,7 +171,6 @@ $(LOCAL_PATH)/prebuilts/audio/vendor/etc/audio_effects.conf:system/vendor/etc/au
 
 # Media
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
